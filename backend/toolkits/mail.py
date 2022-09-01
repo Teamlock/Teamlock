a0c4c9bff535507db6b36b2c5ebc7bee0b5055d4
Olivier de Régis <olivier.de-regis@dc-communication.eu>
2022-09-01 15:02:41 +0200

    Fix Mail wording

--- backend/toolkits/mail.py
+++ backend/toolkits/mail.py
@@ -73,7 +73,7 @@ If these connections are not coming from you, please alert your IT Administrator
             "context": {
                 "link": False,
                 "text": """
-Hello,<br/><br/>You're Teamlock password has been changed.<br/>
+Hello,<br/><br/>Your Teamlock password has been changed.<br/>
 If you did not make this change, please alert your IT Administrator to lock up your account"""
             }
         },
